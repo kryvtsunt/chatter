4e91c9ff599c32d84a7fd3f611a1773001e46525
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-11-02 12:28:27 -0400

    implemented simple db using text file, docummnted, fully tested

---
+++ Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/server/PrattleDBTest.java
@@ -0,0 +1,30 @@
+package edu.northeastern.ccs.im.server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests the simple "data base"(text-file) PrattleDB
+ */
+class PrattleDBTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testDB() throws IOException {
+        PrattleDB db = PrattleDB.instance();
+        db.create("tim", "1234");
+        db.delete("tim");
+        db.create("tim", "1234");
+        db.update("tim", "12345");
+        assertEquals("12345", db.retrieve("tim"));
+        db.delete("tim", "12345");
+        db.create("tim", "timberman_42");
+    }
+}
