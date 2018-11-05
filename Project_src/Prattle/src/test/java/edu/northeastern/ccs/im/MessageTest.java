62881ef2fa74cceee877ecb967769a30bd9cbbfe
sravanthi <sravanthi@ccs.neu.edu>
2018-11-04 19:41:21 -0500

    tests

--- Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/MessageTest.java
+++ Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/MessageTest.java
@@ -17,6 +17,16 @@ class MessageTest {
         assertFalse(message.isAcknowledge());
         assertFalse(message.isInitialization());
         assertFalse(message.terminate());
+
+        Message msg1 = Message.makeBroadcastMessage("Usr","Hello");
+        assertEquals("BCT 3 Usr 5 Hello", msg1.toString());
+
+        Message msg2 = Message.makeNoAcknowledgeMessage();
+        assertEquals("NAK 2 -- 2 --",msg2.toString());
+        assertFalse(msg2.isDirectMessage());
+
+        Message msg3 = Message.makeAcknowledgeMessage("usr");
+        assertEquals("ACK 3 usr 2 --", msg3.toString());
     }
 
 }
