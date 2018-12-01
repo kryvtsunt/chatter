d853a145e68aa817266c7efc8bccbefb80ce460a
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-12-01 17:34:20 -0500

    pc fix

--- Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/ParentControlTest.java
+++ Project_src/Prattle/src/test/java/edu/northeastern/ccs/im/ParentControlTest.java
@@ -1,33 +1,18 @@
 package edu.northeastern.ccs.im;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
-@RunWith(MockitoJUnitRunner.class)
 class ParentControlTest {
 
-    @Mock
-    private BufferedReader bufferedReader;
 
     @Test
     void filterBadWords() {
         ParentControl pc = ParentControl.getInstance();
         assertEquals("***", pc.filterBadWords("ass"));
-        pc.getInstance();
-
     }
 
 }
